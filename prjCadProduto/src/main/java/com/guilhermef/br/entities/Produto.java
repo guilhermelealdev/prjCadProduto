2d8bff06027592be2e8ee297cd64a8e099f3197b
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2026-03-18 09:22:53 -0300

    Mudanças no front e nas entidades
    Entidade produtos com novas variáveis e adaptação no js para funcionar com as novas mudanças

--- prjCadProduto/src/main/java/com/guilhermef/br/entities/Produto.java
+++ prjCadProduto/src/main/java/com/guilhermef/br/entities/Produto.java
@@ -8,30 +8,61 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_produtos")
+@Table(name = "tb_produtos")
 public class Produto {
-	
+
 	@Id
-	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String nome;
 	private double preco;
 	private String descricao;
-	
+	private String brand;
+	private int stock;
+	private String category;
+
 	@Lob
 	private byte[] imagem;
 
 	public Produto() {
-		
+
 	}
-	
-	public Produto(Long id, String nome, double preco, String descricao, byte[] imagem) {
+
+	public Produto(Long id, String nome, double preco, String descricao, String brand, int stock, String category,
+			byte[] imagem) {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
 		this.descricao = descricao;
+		this.brand = brand;
+		this.stock = stock;
+		this.category = category;
 		this.imagem = imagem;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -69,9 +100,5 @@ public class Produto {
 	public void setImagem(byte[] imagem) {
 		this.imagem = imagem;
 	}
-	
-	
-	
-	
-	
+
 }
--- prjCadProduto/target/classes/static/script.js
+++ prjCadProduto/target/classes/static/script.js
@@ -1,48 +1,114 @@
-document.addEventListener("DOMContentLoaded",()=>{
-   const cadastroForm = document.getElementById("cadastroForm");
-   
-   const pesquisaForm = document.getElementById("pesquisaForm");
-   
-   cadastroForm.addEventListener("submit",cadastrarJogo);
-   
-   pesquisaForm.addEventListener("submit",function(e){
-       e.preventDefault();
-       pesquisarJogo();
-   });
+document.addEventListener("DOMContentLoaded", () => {
+  const cadastroForm = document.getElementById("cadastroForm");
+
+  const pesquisaForm = document.getElementById("pesquisaForm");
+
+  cadastroForm.addEventListener("submit", cadastrarJogo);
+
+  pesquisaForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    pesquisarProduto();
+  });
 });
 
+function cadastrarProduto(event) {
+  event.preventDefault();
 
-function cadastrarJogo(event){
-   event.preventDefault();
-   
-   const name = document.getElementById("name").value;
-   const platform = document.getElementById("platform").value;
-   const price = document.getElementById("price").value;
-   const category = document.getElementById("category").value;
-   const thumbnail=document.getElementById("thumbnail").files[0];
-   
-   const jogo = {
-       name:name,
-       platform:platform,
-       price:price,
-       category:category        
-   }
-   
-   const formData = new FormData();
-   
-   formData.append(
-       "jogo",
-       new Blob([JSON.stringify(jogo)],{type:"application/json"})
-   );
-   
-   formData.append("thumbnail",thumbnail)
-   
-   fetch("http://localhost:8080/jogos",{
-       method:"POST",
-       body:formData
-   })
-   .then(data=>{
-       alert("Jogo Cadastrado com Sucesso");
-   })
-   .catch(error=>console.error(error))
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const category = document.getElementById("category").value;
+  const brand = document.getElementById("brand").value;
+  const stock = document.getElementById("stock").value;
+  const description = document.getElementById("description").value;
+  const thumbnail = document.getElementById("thumbnail").files[0];
+
+  const produto = {
+    name: name,
+    price: price,
+    brand: brand,
+	category: category,
+	stock: stock,
+	description: description,
+  };
+
+  const formData = new FormData();
+
+  formData.append(
+    "produto",
+    new Blob([JSON.stringify(produto)], { type: "application/json" }),
+  );
+
+  formData.append("thumbnail", thumbnail);
+
+  fetch("http://localhost:8080/jogos", {
+    method: "POST",
+    body: formData,
+  })
+    .then((data) => {
+      alert("Jogo Cadastrado com Sucesso");
+    })
+    .catch((error) => console.error(error));
+}
+
+function excluirProduto() {
+  const searchId = document.getElementById("searchId").value;
+  if (searchId === "") {
+    alert("Digite um ID para exluir");
+    return;
+  }
+  fetch(`http://localhost:8080/jogos/${searchId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao exluir");
+      }
+      alert("Jogo Excluido com sucesso");
+      document.getElementById("pesquisaForm").reset();
+      document.getElementById("resultadoPesquisa").innerHTML = "";
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Erro ao excluir o Jogo");
+    });
+}
+
+function pesquisarProduto() {
+  const searchId = document.getElementById("searchId").value;
+
+  if (searchId === "") {
+    alert("Digite um ID");
+    return;
+  }
+
+  fetch(`http://localhost:8080/jogos/${searchId}`)
+    .then((response) => {
+      if (response.status === 404) {
+        throw new Error("Jogo não encontrado");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("name").value = data.name;
+      document.getElementById("brand").value = data.brand;
+      document.getElementById("price").value = data.price;
+      document.getElementById("category").value = data.category;
+	  document.getElementById("stock").value = data.stock;
+	document.getElementById("description").value = data.description;
+
+      const resultadoPesquisa = document.getElementById("resultadoPesquisa");
+
+      resultadoPesquisa.innerHTML = `
+<h3>ID: ${data.id}</h3>
+<img style="max-width:200px" src="data:image/jpeg;base64,${data.thumbnail}" alt="thumbnail do produto ${data.name}">
+`;
+    })
+    .catch((error) => {
+      console.error(error);
+
+      const resultadoPesquisa = document.getElementById("resultadoPesquisa");
+
+      resultadoPesquisa.innerHTML = "Produto não encontrado. Inserir ID válido";
+    });
 }
